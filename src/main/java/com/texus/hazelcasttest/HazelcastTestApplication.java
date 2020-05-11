f737c4c2f5f5fca3404e791f82f2ed3d5cd6ebdc
terance123 <terance.zincat@gmail.com>
2020-05-11 22:11:05 +0530

    Initial commit

---
+++ src/main/java/com/texus/hazelcasttest/HazelcastTestApplication.java
@@ -0,0 +1,13 @@
+package com.texus.hazelcasttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HazelcastTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HazelcastTestApplication.class, args);
+	}
+
+}
